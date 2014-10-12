c6d44b7863b7fd0d0537154d8003bef511fcfd52
faydzen <noreply>
2014-10-12 11:55:15 +0000

    Add component btn1

---
+++ ui/btn1.reel/btn1.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/btn1.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Btn1
+ * @extends Component
+ */
+exports.Btn1 = Component.specialize(/** @lends Btn1# */ {
+    constructor: {
+        value: function Btn1() {
+            this.super();
+        }
+    }
+});
